e3d668e25a65549ca0bbc2bb35a8266f3efe58cc
Alexandre Dumont <alexandre@sogilis.com>
2015-12-16 14:32:53 +0100

    Wip 2

--- latlong-example/lat_long.adb
+++ latlong-example/lat_long.adb
@@ -1,6 +1,13 @@
 package body Lat_Long with SPARK_Mode is
 
-   function Distance (Source, Destination : Coordinates) return Float is
-     (0.0);
+   function Distance (Source, Destination : Coordinates) return Float_With_Approx
+   is
+      Delta_Lat : Float_With_Approx;
+      Delta_Long : Float_With_Approx;
+   begin
+      Delta_Lat := (Destination.Lat - Source.Lat) * R * Conv_Deg_To_Rad;
+      Delta_Long := (Destination.Long - Source.Long) * R / Cos(Source.Lat);
+      return Sqrt(Delta_Lat ** 2 + Delta_Long ** 2);
+   end Distance;
 
 end Lat_Long;
--- latlong-example/lat_long.ads
+++ latlong-example/lat_long.ads
@@ -2,31 +2,42 @@ with Ada.Numerics, Ada.Numerics.Elementary_Functions;
 use Ada.Numerics, Ada.Numerics.Elementary_Functions;
 
 package Lat_Long with SPARK_Mode is
-   R : constant Float := 6_367_444.7;
-   Conv_Deg_To_Rad : constant Float := Pi / 180.0;
 
-   subtype Latitude is Float range - 90.0 .. 90.0;
-   subtype Longitude is Float range Float'Succ(- 180.0) .. 180.0;
+   type Float_With_Approx is new Float;
+   function "<" (Left, Right: Float_With_Approx) return Boolean is
+     (Float(Left) < Float(Right) + 10.0E-6);
+
+   R : constant Float_With_Approx := 6_367_444.7;
+   Conv_Deg_To_Rad : constant Float_With_Approx := Pi / 180.0;
+
+   function Cos(X : Float_With_Approx) return Float_With_Approx
+   is (Float_With_Approx(Cos(Float(X))));
+
+   function Sqrt(X : Float_With_Approx) return Float_With_Approx
+   is (Float_With_Approx(Sqrt(Float(X))));
+
+   subtype Latitude is Float_With_Approx range - 90.0 .. 90.0;
+   subtype Longitude is Float_With_Approx range Float_With_Approx'Succ(- 180.0) .. 180.0;
 
    type Coordinates is record
       Lat : Latitude;
       Long : Longitude;
    end record;
 
-   function Distance(Source, Destination : Coordinates) return Float with
+   function Distance(Source, Destination : Coordinates) return Float_With_Approx with
      Pre => Cos(Source.Lat) /= 0.0,
      Post => Distance'Result = Sqrt(
                Delta_Lat_In_Meters(Source, Destination) *  Delta_Lat_In_Meters(Source, Destination) +
                Delta_Long_In_Meters(Source, Destination) * Delta_Long_In_Meters(Source, Destination));
 
-   function Delta_Lat_In_Meters(Source, Destination : Coordinates) return Float
+   function Delta_Lat_In_Meters(Source, Destination : Coordinates) return Float_With_Approx
    is ((Destination.Lat - Source.Lat) * R * Conv_Deg_To_Rad)
    with Ghost,
-     Post => abs(Delta_Lat_In_Meters'Result) <= (Pi * R);
+     Post => abs(Delta_Lat_In_Meters'Result) < (Pi * R);
 
-   function Delta_Long_In_Meters(Source, Destination : Coordinates) return Float
+   function Delta_Long_In_Meters(Source, Destination : Coordinates) return Float_With_Approx
    is ((Destination.Long - Source.Long) * R / Cos(Source.Lat))
    with Ghost,
-     Post => abs(Delta_Long_In_Meters'Result) <= (2.0 * Pi * R);
+     Post => abs(Delta_Long_In_Meters'Result) < (2.0 * Pi * R);
 
 end Lat_Long;
